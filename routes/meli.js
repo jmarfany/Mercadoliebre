9fd12e87ad290b978fece071ea7ba14a45b2ce84
Mr.Marfany <juan.marfany@gmail.com>
2020-10-07 12:21:29 -0300

    agrego ingreso

--- routes/meli.js
+++ routes/meli.js
@@ -5,8 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render("meli");
 });
+
 router.get('/registro', function(req, res, next) {
   res.render("registro");
+});
+
+router.get('/ingreso', function(req, res, next) {
+  res.render("ingreso");
 });
 
 
